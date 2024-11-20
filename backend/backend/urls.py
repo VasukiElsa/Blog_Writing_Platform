b69bd686207f8158b9965c937c8c7c4195448508
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-11-20 23:17:48 +0530

    Added media urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('headers/', views.headerView),
     path('inputfields/',views.inputfieldView),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
